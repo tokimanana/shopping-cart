01c0635a366da9cef8b73951bdb4297b754cd0b5
tokimanana <tokimananasarobidy@gmail.com>
2025-11-19 02:04:43 +0300

    fix: create effect

--- src/app/products/product.effects.ts
+++ src/app/products/product.effects.ts
@@ -1,7 +1,6 @@
 import { inject, Injectable } from '@angular/core';
-import { Actions, ofType } from '@ngrx/effects';
+import { Actions, ofType, createEffect } from '@ngrx/effects';
 import { ProductService } from './product.service';
-
 import { ProductActions } from './product.actions';
 import { catchError, exhaustMap, map, of } from 'rxjs';
 
@@ -10,16 +9,18 @@ export class ProductEffects {
   private action$ = inject(Actions);
   private productService = inject(ProductService);
 
-  loadProducts$ = this.action$.pipe(
-    ofType(ProductActions.loadProducts),
-    exhaustMap(() =>
-      this.productService.getProducts().pipe(
-        map((products) => {
-          return ProductActions.loadProductsSuccess({ products });
-        }),
-        catchError((error) => {
-          return of(ProductActions.loadProductsFailure({ error }));
-        })
+  loadProducts$ = createEffect(() =>
+    this.action$.pipe(
+      ofType(ProductActions.loadProducts),
+      exhaustMap(() =>
+        this.productService.getProducts().pipe(
+          map((products) => {
+            return ProductActions.loadProductsSuccess({ products });
+          }),
+          catchError((error) => {
+            return of(ProductActions.loadProductsFailure({ error }));
+          })
+        )
       )
     )
   );
