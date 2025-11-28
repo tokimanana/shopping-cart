70589d5e5daf947bcfe5e361727f59348fb7a63d
tokimanana <tokimananasarobidy@gmail.com>
2025-11-28 23:50:00 +0300

    feat: implement product list with NgRx

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,40 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import {
+  selectAllProducts,
+  selectProductsError,
+  selectProductsLoading,
+} from '../products/product.selector';
+import { Product } from '../products/product.model';
+import { ProductActions } from '../products/product.actions';
+import { CartActions } from '../cart/cart.action';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'app-product-list',
+  imports: [CommonModule],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css',
+})
+export class ProductListComponent implements OnInit {
+  private readonly store = inject(Store);
+
+  readonly products$ = this.store.select(selectAllProducts);
+  readonly isLoading$ = this.store.select(selectProductsLoading);
+  readonly error$ = this.store.select(selectProductsError);
+
+  ngOnInit(): void {
+    this.store.dispatch(ProductActions.loadProducts());
+  }
+
+  addToCart(productId: string): void {
+    this.store.dispatch(CartActions.addItem({ productId }));
+  }
+
+  onImageError(event: Event): void {
+    const img = event.target as HTMLImageElement;
+    img.src = 'https://placehold.co/300x200/cccccc/ffffff?text=Image+Error';
+  }
+}
