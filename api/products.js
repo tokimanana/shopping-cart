5da8646b6f7e06f0234fd8f6042ad0fdde035246
tokimanana <tokimananasarobidy@gmail.com>
2025-12-18 09:45:00 +0300

    fix: optimize image URLs to reduce payload size

--- api/products.js
+++ api/products.js
@@ -4,6 +4,6 @@ module.exports = (req, res) => {
   res.setHeader('Content-Type', 'application/json; charset=utf-8');
   res.setHeader('Access-Control-Allow-Origin', '*');
 
-  const products = dbData.products.slice(0, 10);
+  // const products = dbData.products.slice(0, 10);
   res.status(200).send(JSON.stringify(products));
 };
